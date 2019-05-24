b504628c55e7cb73a8c4641e4ee96da80dac9164
Yogeshbhai Chalodiya <ychalodiya@gmail.com>
2019-05-24 13:13:25 -0400

    attach more filter for search items

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function getUrl() {
     // get the input ID
     let tcid = document.getElementById('tcid').value.trim();
     let arrayOfTCID = tcid.split(" ").map(index => {
-    let trimmed_ID = index.replace(/^[\,]+|[\,]+$/gm, "");
-    let ans = /^[a-z|A-Z]+tc{1}$/gm.test(trimmed_ID);
+    let trimmed_ID = index.replace(/^[\,$.@%*&!`~#^-_+=<>?;:'"(){}[]]+|[\,$.@%*&!`~#^-_+=<>?;:'"(){}[]]+$/gm, "");
+    let ans = /^[a-z|A-Z|0-9]+tc{1}$/gmi.test(trimmed_ID);
         // let ans = /^[a-z|A-Z]+[^tc]\s?tc{1}$/gm.test(trimmed_ID);
         // create a link if it finds right tcid
 
@@ -12,7 +12,7 @@ function getUrl() {
         if (ans) {
             // let composerUrl = `http://${trimmed_ID}.composer.dealersmartsolutions.ca/website/as/${trimmed_ID}/${trimmed_ID}-admin/composer/index?lang=en_CA#website`;
 
-            let welcomeURL = `https://apps.dealersmartsolutions.ca/welcome/as/${trimmed_ID}/${trimmed_ID}-admin/dashboard/index?lang=en_CA`;
+           let welcomeURL = `https://apps.dealersmartsolutions.ca/welcome/as/${trimmed_ID}/${trimmed_ID}-admin/dashboard/index?lang=en_CA`;
 
             let tclink = document.getElementById('tclink');
             tclink.style.display = 'block';
@@ -20,6 +20,7 @@ function getUrl() {
             let linkaddress = document.createTextNode(trimmed_ID);
             a.appendChild(linkaddress);
             // a.href = composerUrl;
+
             a.href = welcomeURL;
             a.setAttribute('target', '_blank');
             a.className = 'link';
@@ -46,5 +47,3 @@ function getMedia(mediaID) {
     let tcMedia = document.getElementById('tcMedia');
     tcMedia.style.display = 'block';
 }
-
-// example: ,,cmpautomotiveltdtc , countryhillshyundaitc , countryhillsnissantc ,
